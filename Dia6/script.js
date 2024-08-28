324ac97fbf31f92e2d64c4c5e7ead1168cab34a8
zu0910 <zullyortiz.09@gmail.com>
2024-08-28 17:48:48 -0500

    feat :sparkles: continuo to the code

--- Dia6/script.js
+++ Dia6/script.js
@@ -103,3 +103,107 @@ function OrdenesJson(){
 OrdenesJson();
 
 
+//document.getElementById("hola").addEventListener("click",mostrar);
+let info = [];
+fetch("info.json")
+.then(res =>res.json())
+.then(data => {
+    info.push(data)
+})
+
+function obtener_producto(){
+    let id = document.getElementById("id").value;
+    let name = document.getElementById("name").value;
+    let category = document.getElementById("category").value;
+    let price = document.getElementById("price").value;
+    let quantityInStock = document.getElementById("quantityInStock").value;
+    let supplierId = document.getElementById("supplierId").value;
+    let lista_productos_agregar={"id":id,"name":name,"category":category,"price":price,"quantityInStock":quantityInStock,"supplierId":supplierId};
+    addProduct(lista_productos_agregar);
+}
+function addProduct(product){//función que agrega un nuevo producto
+    let contador=0;
+    let contador_p=0;
+    for (const x of info[0]["products"]){
+        if (product["id"]==x["id"]) {
+            contador_p+=1;
+        }
+    }
+    for (const i of info[0]["suppliers"]){
+        if (product["supplierId"]==i["id"]) {//verifica si el proveedor existe
+            contador+=1
+        }
+    }
+    if (contador>0 && contador_p==0){//si el proveedor existe se agrega el nuevo producto
+        info[0]["products"].push(product)
+    }
+    if (contador_p>0) {
+        alert("ya existe un producto con este id")
+    }
+    if (contador==0) {
+        alert("no se encontró el proveedor que especificaste")
+    }
+}
+document.getElementById("ver_productos").addEventListener("click",viewProducts)
+function viewProducts(){//función que muestra todos los productos
+    document.getElementById("mostrar_productos").innerHTML =""
+    for (const i of info[0]["products"]){
+        document.getElementById("mostrar_productos").innerHTML +=
+        `<p>----------------------------</p>
+        <p>id: ${i["id"]}</p>
+        <p>name: ${i["name"]}</p>
+        <p>category: ${i["category"]}</p>
+        <p>price: ${i["price"]}</p>
+        <p>quantityInStock: ${i["quantityInStock"]}</p>
+        <p>supplierId: ${i["supplierId"]}</p>
+        <p>----------------------------</p>`
+    }
+}
+function actualizar_producto(){
+    let id = document.getElementById("id_actualizar").value;
+    let name = document.getElementById("nombre_actualizar").value;
+    let category = document.getElementById("categoria_actualizar").value;
+    let price = document.getElementById("precio_actualizar").value;
+    let quantityInStock = document.getElementById("cantidad_actualizar").value;
+    let actualizar_producto={"name":name,"category":category,"price":price,"quantityInStock":quantityInStock};
+    updateProduct(id,actualizar_producto)
+}
+function updateProduct(id, newDetails){//función que actualiza los detalles de un producto
+    let contador = 0;
+    for (const i of info[0]["products"]){
+        if (id==i["id"]) {
+            contador+=1;
+            if (newDetails["name"]!="") {
+                i["name"]=newDetails["name"]
+            }
+            if (newDetails["category"]!="") {
+                i["category"]=newDetails["category"]
+            }
+            if (newDetails["price"]!="") {
+                i["price"]=newDetails["price"]
+            }
+            if (newDetails["quantityInStock"]!="") {
+                i["quantityInStock"]=newDetails["quantityInStock"]
+            }
+        }
+    }
+    if (contador==0) {
+        alert("el id que ingresaste no existe");
+    }
+}
+function eliminar_producto(){
+    let id = document.getElementById("id_eliminar").value;
+    deleteProduct(id);
+}
+function deleteProduct(id){//función que elinina un producto especificado por el usuario
+    let contador = 0;
+    for (let i=info[0]["products"].length-1; i >= 0; i--){
+        if (id==info[0]["products"][i]["id"]) {
+            info[0]["products"].splice(i,1);
+            contador+=1;
+        }
+    }
+    if (contador==0) {
+        alert("El id que ingresaste no existe");
+    }
+}
