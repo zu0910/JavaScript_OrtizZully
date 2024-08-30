c3ed104dc25d129af310c8b25e1417299a6fda45
zu0910 <zullyortiz.09@gmail.com>
2024-08-30 17:58:07 -0500

    feat :sparkles: usage of species and films done

--- Dia8/script.js
+++ Dia8/script.js
@@ -45,29 +45,152 @@ function view(link){
                 <td>${data.gender}</td>
             </tr>
 
-            <tr class="table-dark">
-                <td scope="row">Homeworld: </td>
-                <td>
-                    <ul class="list-group">
-                        <li class="list-group-item"><strong>Name: ${data.homeworld.name}</strong></li>
-                        <li class="list-group-item"><strong>Rotation period: ${data.homeworld}</strong></li>
-                        <li class="list-group-item"><strong>Orbital period: ${data.homeworld}</strong></li>
-                        <li class="list-group-item"><strong>Dilameter: ${data.homeworld}</strong></li>
-                        <li class="list-group-item"><strong>Climate: ${data.homeworld}</strong></li>
-                        <li class="list-group-item"><strong>Gravity: ${data.homeworld}</strong></li>
-                        <li class="list-group-item"><strong>Terrain: ${data.homeworld}</strong></li>
-                        <li class="list-group-item"><strong>Surface water: ${data.homeworld}</strong></li>
-                        <li class="list-group-item"><strong>Population: ${data.homeworld}</strong></li>
-                        <li class="list-group-item"><strong>Created: ${data.homeworld}</strong></li>
-                        <li class="list-group-item"><strong>Edited: ${data.homeworld}</strong></li>
-                        <li class="list-group-item"><strong>Url: ${data.homeworld}</strong></li>
-                    </ul>
-                </td>
-            </tr>
+            
                     
             
         </tbody>
         </table>
         `
+        fetch(data.homeworld)
+        .then(ress => ress.json())
+        .then(homeworld => {
+            document.getElementById("view").innerHTML+=`
+
+            <table class="table">
+                <tbody>
+                    <tr class="table-dark">
+                        <td scope="row">Homeworld: </td>
+                            <td>
+                                <ul class="list-group">
+                                    <li class="list-group-item">
+                                        <table class="table table-light">
+                                            <tbody>
+                                                <tr><th>Name:</th><td> ${homeworld.name}</td></tr>
+                                                    <tr><th>Rotation period:</th><td> ${homeworld["rotation_period"]}</td></tr>
+                                                    <tr><th>Orbital period:</th><td> ${homeworld["orbital_period"]}</td></tr>
+                                                    <tr><th>Dilameter:</th><td> ${homeworld.diameter}</td></tr>
+                                                    <tr><th>Climate:</th><td> ${homeworld.climate}</td></tr>
+                                                    <tr><th>Gravity:</th><td> ${homeworld.gravity}</td></tr>
+                                                    <tr><th>Terrain:</th><td> ${homeworld.terrain}</td></tr>
+                                                    <tr><th>Surface water:</th><td> ${homeworld["surface_water"]}</td></tr>
+                                                    <tr><th>Population:</th><td> ${homeworld.population}</td></tr>
+                                                    <tr><th>Residents:</th><td> ${homeworld.residents.map(i=>i).join("<br>")}</td></tr>
+                                                    <tr><th>Films:</th><td> ${homeworld.films.map(i=>i).join("<br>")}</td></tr>
+                                                    <tr><th>Created:</th><td> ${homeworld.created}</td></tr>
+                                                    <tr><th>Edited:</th><td> ${homeworld.edited}</td></tr>
+                                                    <tr><th>Url:</th><td> ${homeworld.url}</td></tr>
+                                            </tbody>
+                                        </table>
+                                    </li>
+                                </ul>
+                            </td>
+                        </td>
+                    </tr>
+                    <tr class="table-dark">
+                        <td scope="row">Films: </td>
+                            <td>
+                                <ul>
+                                    <li id="fil" class="list-group-item">
+                                        <table>
+                                            <tbody>
+                                                <table class="table table-light">
+                                                </table>
+                                            </tbody>
+                                        </table>
+                                    </li>
+                                </ul>
+                            </td>
+                        </td>
+                    </tr>
+                    <tr class="table-dark">
+                        <td scope="row">Species: </td>
+                            <td>
+                                <ul>
+                                    <li id="specie" class="list-group-item">
+                                        <table>
+                                            <tbody>
+                                                <table class="table table-light">
+
+                                                </table>
+                                            </tbody>
+                                        </table>
+                                    </li>
+                                </ul>
+                            </td>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+             
+            `
+        })
+        
+        for ( const i of data.films ){
+            fetch(i)
+            .then(rest => rest.json())
+            .then(films => {
+                document.getElementById("fil").innerHTML+=`
+               
+                <table>
+                    <tbody>
+                        <table class="table table-light">
+                            <tr><th>Name:</th><td> ${films.title}</td></tr>
+                            <tr><th>Rotation period:</th><td> ${films["episode_id"]}</td></tr>
+                            <tr><th>Orbital period:</th><td> ${films["opening_crawl"]}</td></tr>
+                            <tr><th>Dilameter:</th><td> ${films.director}</td></tr>
+                            <tr><th>Climate:</th><td> ${films.producer}</td></tr>
+                            <tr><th>Gravity:</th><td> ${films["release_date"]}</td></tr>
+                            <tr><th>Terrain:</th><td> ${films.characters.map(i=>i).join("<br>")}</td></tr>
+                            <tr><th>Surface water:</th><td> ${films.planets.map(i=>i).join("<br>")}</td></tr>
+                            <tr><th>Population:</th><td> ${films.starships.map(i=>i).join("<br>")}</td></tr>
+                            <tr><th>Residents:</th><td> ${films.vehicles.map(i=>i).join("<br>")}</td></tr>
+                            <tr><th>Films:</th><td> ${films.species.map(i=>i).join("<br>")}</td></tr>
+                            <tr><th>Created:</th><td> ${films.created}</td></tr>
+                            <tr><th>Edited:</th><td> ${films.edited}</td></tr>
+                            <tr><th>Url:</th><td> ${films.url}</td></tr>
+                        </table>
+                    </tbody>
+                </table>            
+                
+                
+                `
+            })
+        }
+
+        for ( const i of data.species){
+            fetch(i)
+            .then(ress => ress.json())
+            .then(species => {
+                document.getElementById("specie").innerHTML+=`
+
+                <table>
+                    <tbody>
+                        <table class="table table-light">
+                            <tr><th>Name:</th><td> ${species.name}</td></tr>
+                            <tr><th>Classification:</th><td> ${species.classification}</td></tr>
+                            <tr><th>Designation:</th><td> ${species.designation}</td></tr>
+                            <tr><th>Average height:</th><td> ${species.average_height}</td></tr>
+                            <tr><th>skin colors:</th><td> ${species.skin_colors}</td></tr>
+                            <tr><th>hair colors:</th><td> ${species.hair_colors}</td></tr>
+                            <tr><th>Eye colors:</th><td> ${species.eye_colors}</td></tr>
+                            <tr><th>Average lifespan:</th><td> ${species.average_lifespan}</td></tr>
+                            <tr><th>Homeworld:</th><td> ${species.homeworld}</td></tr>
+                            <tr><th>Language:</th><td> ${species.language}</td></tr>
+                            <tr><th>People:</th><td> ${species.people.map(i=>i).join("<br>")}</td></tr>
+                            <tr><th>Films:</th><td> ${species.films.map(i=>i).join("<br>")}</td></tr>
+                            <tr><th>Created:</th><td> ${species.creates}</td></tr>
+                            <tr><th>Edited:</th><td> ${species.edited}</td></tr>
+                            <tr><th>Url:</th><td> ${species.url}</td></tr>
+                        </table>
+                    </tbody>
+                </table> 
+            
+                
+                `
+            })
+        }
+        
+        
+        
     })
 }
