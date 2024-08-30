1d4ec34489d7efececdb5a7c1a659b4b7c808c8c
zu0910 <zullyortiz.09@gmail.com>
2024-08-30 10:08:34 -0500

    feat :sparkles: API done

--- Dia7/script.js
+++ Dia7/script.js
@@ -12,26 +12,27 @@ function view(link){
         document.getElementById("view").innerHTML+=`
         <center>
         
-            <div class="card" style="width: 25vw; border: 0.1vw solid black; box-shadow: 0 0 20px black; margin-top: 2vw; margin-left: 5vw">
+            <div class="card" style="width: 25vw;  box-shadow: 0 0 20px black; margin-top: 2vw; margin-left: 5vw">
 
                 <img src="${data.image.url}" class="card-img-top" style="width:25vw; height: 20vw">
 
                 <div class="card-body">
 
                     <h5 class="card-title" style=" font-weight: bold; font-size: 2vw;">${data.name}</h5>
+                    <h5 class="card-title"> ID ${data.id}</h5>
 
                 </div>
                 
                 <ul class="list-group list-group-flush" >
-                    <li class="list-group-item" style=" border: 0.01vw solid black">Intelligence: ${data.powerstats.intelligence} / strength: ${data.powerstats.strength} / speed: ${data.powerstats.speed} / durability: ${data.powerstats.durability} / power: ${data.powerstats.power} / combat: ${data.powerstats.combat}</li>
+                    <li class="list-group-item" >Intelligence: ${data.powerstats.intelligence} <br> strength: ${data.powerstats.strength} <br> speed: ${data.powerstats.speed} <br> durability: ${data.powerstats.durability} <br> power: ${data.powerstats.power} <br> combat: ${data.powerstats.combat}</li>
 
-                    <li class="list-group-item" style=" border: 0.01vw solid black" ><h6>Biography</h6> Full name:  ${data.biography["full-name"]} / alter-egos:  ${data.biography["alter-egos"]} / aliases:  ${data.biography["aliases"]} / place-of-birth:  ${data.biography["place-of-birth"]} / first-appearance:  ${data.biography["first-appearance"]} / publisher:  ${data.biography["publisher"]} / alignment:  ${data.biography["alignment"]} </li>
+                    <li class="list-group-item"  ><h6>Biography</h6> Full name:  ${data.biography["full-name"]} <br> alter-egos:  ${data.biography["alter-egos"]} <br> aliases:  ${data.biography["aliases"]} <br> place-of-birth:  ${data.biography["place-of-birth"]} <br> first-appearance:  ${data.biography["first-appearance"]} <br> publisher:  ${data.biography["publisher"]} <br> alignment:  ${data.biography["alignment"]} </li>
 
-                    <li class="list-group-item" style=" border: 0.01vw solid black" > <h6>appearance</h6> / gender: ${data.appearance.gender} / race: ${data.appearance.race} / height: ${data.appearance.height} / height: ${data.appearance.height} / weight: ${data.appearance.weight} / eye-color: ${data.appearance["eye-color"]} / hair-color: ${data.appearance["hair-color"]} </li>
+                    <li class="list-group-item"  > <h6>appearance</h6> gender: ${data.appearance.gender} <br> race: ${data.appearance.race} <br> height: ${data.appearance.height} <br> height: ${data.appearance.height} <br> weight: ${data.appearance.weight} <br> eye-color: ${data.appearance["eye-color"]} <br> hair-color: ${data.appearance["hair-color"]} </li>
 
-                    <li class="list-group-item" style=" border: 0.01vw solid black">connections ${data.connections["group-affiliation"]} / Relatives ${data.connections.relatives}</li>
+                    <li class="list-group-item" > <h6>connections</h6> ${data.connections["group-affiliation"]} <br> Relatives ${data.connections.relatives}</li>
 
-                    <li class="list-group-item" style=" border: 0.01vw solid black">ocupation: ${data.work.occupation} / base: ${data.work.base}</li>
+                    <li class="list-group-item" > <h6>ocupation</h6> ${data.work.occupation} <br> base: ${data.work.base}</li>
                     
                 </ul>
             </div>
