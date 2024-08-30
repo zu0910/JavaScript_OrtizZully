841475b24cb6c032f9900a063528edd83f41e7d2
zu0910 <zullyortiz.09@gmail.com>
2024-08-29 23:05:59 -0500

    feat :sparkles: API view done

--- Dia7/script.js
+++ Dia7/script.js
@@ -10,24 +10,32 @@ function view(link){
     .then(res => res.json())
     .then(data => {
         document.getElementById("view").innerHTML+=`
-        <div class="card" style="width: 20vw; border: 0.1vw solid black; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
-            <img src="${data.image.url}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${data.name}</h5>
-                <p class="card-text">connections: <br> ${data.connections["group-affiliation"]}</p>
-                <p class="card-text">Relatives: <br> ${data.connections.relatives}</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Intelligence: ${data.powerstats.intelligence} <br>strength: ${data.powerstats.strength} <br> speed: ${data.powerstats.speed} <br> durability: ${data.powerstats.durability} <br> power: ${data.powerstats.power} <br> combat: ${data.powerstats.combat}</li>
+        <center>
+        
+            <div class="card" style="width: 25vw; border: 0.1vw solid black; box-shadow: 0 0 20px black; margin-top: 2vw; margin-left: 5vw">
 
-                <li class="list-group-item">Biography: <br> Full name:  ${data.biography["full-name"]} <br> alter-egos:  ${data.biography["alter-egos"]} <br> aliases:  ${data.biography["aliases"]} <br> place-of-birth:  ${data.biography["place-of-birth"]} <br> first-appearance:  ${data.biography["first-appearance"]} <br> publisher:  ${data.biography["publisher"]} <br> alignment:  ${data.biography["alignment"]} </li>
+                <img src="${data.image.url}" class="card-img-top" style="width:25vw; height: 20vw">
 
-                <li class="list-group-item"> appearance: <br> gender: ${data.appearance.gender} <br> race: ${data.appearance.race} <br> height: ${data.appearance.height} <br> height: ${data.appearance.height} <br> weight: ${data.appearance.weight} <br> eye-color: ${data.appearance["eye-color"]} <br> hair-color: ${data.appearance["hair-color"]} </li>
+                <div class="card-body">
 
-                <li class="list-group-item">ocupation: ${data.work.occupation} <br> ocupation: ${data.work.base}</li>
+                    <h5 class="card-title" style=" font-weight: bold; font-size: 2vw;">${data.name}</h5>
+
+                </div>
                 
-            </ul>
-        </div>
+                <ul class="list-group list-group-flush" >
+                    <li class="list-group-item" style=" border: 0.01vw solid black">Intelligence: ${data.powerstats.intelligence} / strength: ${data.powerstats.strength} / speed: ${data.powerstats.speed} / durability: ${data.powerstats.durability} / power: ${data.powerstats.power} / combat: ${data.powerstats.combat}</li>
+
+                    <li class="list-group-item" style=" border: 0.01vw solid black" ><h6>Biography</h6> Full name:  ${data.biography["full-name"]} / alter-egos:  ${data.biography["alter-egos"]} / aliases:  ${data.biography["aliases"]} / place-of-birth:  ${data.biography["place-of-birth"]} / first-appearance:  ${data.biography["first-appearance"]} / publisher:  ${data.biography["publisher"]} / alignment:  ${data.biography["alignment"]} </li>
+
+                    <li class="list-group-item" style=" border: 0.01vw solid black" > <h6>appearance</h6> / gender: ${data.appearance.gender} / race: ${data.appearance.race} / height: ${data.appearance.height} / height: ${data.appearance.height} / weight: ${data.appearance.weight} / eye-color: ${data.appearance["eye-color"]} / hair-color: ${data.appearance["hair-color"]} </li>
+
+                    <li class="list-group-item" style=" border: 0.01vw solid black">connections ${data.connections["group-affiliation"]} / Relatives ${data.connections.relatives}</li>
+
+                    <li class="list-group-item" style=" border: 0.01vw solid black">ocupation: ${data.work.occupation} / base: ${data.work.base}</li>
+                    
+                </ul>
+            </div>
+        </center>
         `
     })
 }
