7f37c062b3422dfb77c76791b7d15ede890138df
zu0910 <zullyortiz.09@gmail.com>
2024-09-03 00:09:34 -0500

    feat :sparkles: image, name, id, already functional buttons of the pokeApi

---
+++ Dia9/script.js
@@ -0,0 +1,35 @@
+document.getElementById("form").addEventListener("submit",poke_id)
+document.getElementById("form").addEventListener("submit",function(e){
+    e.preventDefault();
+})
+function poke_id(){
+    var name_poke = document.getElementById("name_poke").value;
+    look_for(name_poke)
+}
+let id;
+let idApi=1;
+function look_for(name_poke){
+    let url = "https://pokeapi.co/api/v2/pokemon/" + name_poke;
+    fetch(url)
+        .then( res => res.json())
+        .then( data => {
+            document.getElementById("gif_poke").innerHTML = ""
+            document.getElementById("gif_poke").innerHTML += `
+            <img src="${data.sprites.other.showdown.front_default}">
+            <audio autoplay src="${data.cries.latest}">
+            </audio>
+            `
+            document.getElementById("texto").innerHTML=""
+            document.getElementById("texto").innerHTML += `
+            ${data.id} - <p id="name">${data.forms[0].name}</p>
+            `
+            idApi = data.id;
+
+            document.getElementById("prev").addEventListener("click", (e) => {
+                look_for(idApi-1)
+            })
+            document.getElementById("next").addEventListener("click", (e) => {
+                look_for(idApi+1)
+            })
+        })
+}
