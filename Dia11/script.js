760be2ee244fc6722c9b1e7daebd8470832d8939
zu0910 <zullyortiz.09@gmail.com>
2024-09-05 20:58:18 -0500

    feat :sparkles: show text and info

--- Dia11/script.js
+++ Dia11/script.js
@@ -15,10 +15,67 @@ fetch("https://randomuser.me/api/")
         }
     } )
     function aparecer_user(){
+        document.getElementById("negrita").innerHTML = `
+        ${datas[0].results[0].login.username}    
+                                
+        `
         document.getElementById("text").innerHTML = `
-        ${datas[0].results[0].login.username}                            
+        <p>Hi, My name is </p>                     
         `
     }
-    function reset(){
-        document.getElementById("text").innerHTML = ""
+    function aparecer_email(){
+        document.getElementById("negrita").innerHTML = `
+        ${datas[0].results[0].email}  
+        `
+        document.getElementById("text").innerHTML = `
+        <p> My email address is </p>                     
+        `
+    }
+    const birthday = new Date(datas[0].results[0].dob.date);
+        const date = birthday.toLocaleDateString('en-US',{
+            year: "numeric",
+            month: "numeric",
+            day: "numeric"
+        })
+    function aparecer_birth(){
+        document.getElementById("negrita").innerHTML = `
+        ${dateBirth}
+        `
+        document.getElementById("text").innerHTML = `
+        <p> My birthday is </p>                     
+        `
+        const birthday = new Date (datas.results[0].dob.date);
+        const dateBirth = birthday.toLocaleDateString('en-US',{
+            year : 'numeric',
+            month : "numeric",
+            day : "numeric"
+        })
+    }
+    function aparecer_location(){
+        document.getElementById("negrita").innerHTML = `
+        ${datas[0].results[0].location.street.number} ${datas[0].results[0].location.street.name}
+        `
+        document.getElementById("text").innerHTML = `
+        <p>Hi, My address is </p>                     
+        `
+    }
+    function aparecer_phone(){
+        document.getElementById("negrita").innerHTML = `
+        ${datas[0].results[0].phone}  
+        `
+        document.getElementById("text").innerHTML = `
+        <p>Hi, My phone number is </p>                     
+        `
+    }
+    function aparecer_pass(){
+        document.getElementById("negrita").innerHTML = `
+        ${datas[0].results[0].login.password}  
+        `
+        document.getElementById("text").innerHTML = `
+        <p>Hi, My password is </p>                     
+        `
+    }
+    function reset1(){
+        document.getElementById("negrita").innerHTML = ""
+        document.getElementById("text").innerHTML =""
     }
