5aa70a7d0e297dcd62fb8ed63920fdb993506eaa
zu0910 <zullyortiz.09@gmail.com>
2024-09-09 12:48:57 -0500

    feat :sparkles: list of task done

--- Dia12/script.js
+++ Dia12/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const addTaskButton = document.getElementById('addTaskButton');
 
     async function fetchData() {
-        const res = await fetch('https://6674179975872d0e0a950e53.mockapi.io/todoList');
+        const res = await fetch('https://66df33cdde4426916ee3e0e3.mockapi.io/tareas');
         data = await res.json();
         return data;
     }
@@ -51,9 +51,61 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         datosContenedor.appendChild(capDiv)
         });
+        document.querySelectorAll('.completado').forEach(button => {
+            button.addEventListener('click',botoncompletado);
+        });
+        document.querySelectorAll('.eliminado').forEach(button => {
+            button.addEventListener('click', botoneliminado);
+        })
     }
+
+    async function addNewTask() {
+        const task = taskInput.value;
+        if(task.trim()=== ''){
+            return;
+        }
+        // 
+        await fetch('https://66df33cdde4426916ee3e0e3.mockapi.io/tareas',{
+            method: 'POST',
+            headers:{
+                'Content-Type' : 'application/json',
+            },
+            body:JSON.stringify({task,status:'On hold'})
+        });
+        taskInput.value='';
+        const data = await fetchData();
+        displayCapsula(data);
+    }
+    addTaskButton.addEventListener('click',addNewTask);
+    // 
+    async function botoncompletado(event) {
+        const id = event.target.getAttribute('data-id');
+        await fetch(`https://66df33cdde4426916ee3e0e3.mockapi.io/tareas/${id}`,{
+            method: 'PUT',
+            headers:{
+                'Content-Type' : 'application/json',
+            },
+            body:JSON.stringify({status:'ready'})
+        });
+        const data = await fetchData();
+        displayCapsula(data)
+    };
+    //eliminar
+    async function botoneliminado(event) {
+        const id = event.target.getAttribute('data-id');
+        await fetch(`https://66df33cdde4426916ee3e0e3.mockapi.io/tareas/${id}`,{
+            method: 'DELETE',
+            headers:{
+                'Content-Type' : 'application/json',
+            },
+            body:JSON.stringify({status:'ready'})
+        });
+        const data = await fetchData();
+        displayCapsula(data)
+    };
+
     fetchData().then(data => {
-        
+        console.log(data);
         displayCapsula(data);
     })
 })
